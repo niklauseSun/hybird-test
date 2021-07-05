9ce2c18ed5da48c1b0960fcdb210685ed9b605b2
sunyujian <yujian.sun@dmall.com>
2021-07-05 10:18:07 +0800

    feat: 添加配置

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,54 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <div>测试</div>
+    <div class="action-list">
+      <div class="action-button" @click="alertAction">
+        <span>alert</span>
+      </div>
+      <div class="action-button" @click="toastAction">
+        <span>toast</span>
+      </div>
+      <div class="action-button" @click="scanAction">
+        <span>scan</span>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
 export default {
   name: 'App',
-  components: {
-    HelloWorld
+  methods: {
+    alertAction() {
+      quick.ui.alert({
+        title: '标题',
+        message: '信息',
+        buttonName: '确定',
+        cancelable: 1,
+        success: function() {
+          // 点击 alert的按钮后回调
+        },
+        error: function() {}
+      });
+    },
+    toastAction() {
+      quick.ui.toast({
+        message: 'sd#ddd测试',
+        success: function() {},
+        error: function() {}
+      });
+    },
+    scanAction() {
+      console.log('quick.util.selectImage', quick.util);
+      quick.util.scan({
+        success: function(result) {
+          console.log('result', result);
+        },
+        error: function(error) {}
+      });
+    }
   }
-}
+};
 </script>
 
 <style>
@@ -24,5 +59,14 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+}
+
+.action-list {
+  display: flex;
+  flex-direction: column;
+}
+
+.action-button {
+  margin-top: 20px;
 }
 </style>
--- src/main.js
+++ src/main.js
@@ -1,8 +1,30 @@
-import Vue from 'vue'
-import App from './App.vue'
+import Vue from 'vue';
+import App from './App.vue';
 
-Vue.config.productionTip = false
+Vue.config.productionTip = false;
+
+var quick = require('./external/quick.min');
+
+window.quick = quick;
+require('./external/quick.native.min');
+require('./external/quick.h5.min');
+
+Vue.prototype.quick = quick;
+
+console.log('quick', quick);
+
+
+quick.error(function(error) {
+    alert(JSON.stringify(error));
+});
+
+quick.config({
+  // 可以传入需要注册的拓展组件-如果原生有支持的话，也可以不传
+});
+quick.ready(function() {
+    // TODO: 处理验证成功后的事情，例如调用api
+});
 
 new Vue({
-  render: h => h(App),
-}).$mount('#app')
+  render: (h) => h(App)
+}).$mount('#app');
