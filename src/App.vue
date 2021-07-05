e1fbb07bca152565a4645bb8fae577d26bf2ab56
sunyujian <yujian.sun@dmall.com>
2021-07-05 10:20:03 +0800

    feat: 移除无用的代码

--- src/App.vue
+++ src/App.vue
@@ -39,7 +39,6 @@ export default {
       });
     },
     scanAction() {
-      console.log('quick.util.selectImage', quick.util);
       quick.util.scan({
         success: function(result) {
           console.log('result', result);
