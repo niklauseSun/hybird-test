57626e9a943f4f5e239435725adc071011129225
yujian.sun <yujian.sun@dmall.com>
2021-10-01 11:33:08 +0800

    feat: 更新了文件部分的代码

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div>测试</div>
     <div class="action-list">
       <img
-        style="width: 100px;height: 100px"
+        style="width: 100px; height: 100px"
         :src="imageName"
         v-if="imageName"
       />
@@ -73,287 +73,307 @@
 </template>
 
 <script>
-export default {
-  name: 'App',
-  data() {
-    return {
-      imageName: undefined
-    };
-  },
-  mounted() {
-    JSBridge.registerHandler('onChannelJoinChanged', (e) => {
-      console.log('onChannelJoinChanged', e);
-    });
-    JSBridge.registerHandler('onReceiveFromGroup', (e) => {
-      console.log('onReceiveFromGroup', e);
-    });
-    JSBridge.registerHandler('onReceiveFromPeer', (e) => {
-      console.log('onReceiveFromPeer', e);
-    });
-    JSBridge.registerHandler('onCallUserState', (e) => {
-      console.log('onCallUserState', e);
-    });
-    JSBridge.registerHandler('onCallUserMute', (e) => {
-      console.log('onCallUserMute', e);
-    });
-  },
-  methods: {
-    tirggerFile(event) {
-      //  String ACCEPT_IMAGE = "image";
-      // String ACCEPT_CAMERA = "camera";
-      // String ACCEPT_FILE = "file";
-      console.log('triggerFile', event.target.files);
-
-      let fr = new FileReader();
-
-      fr.onload = function(e) {
-        console.log('e', e);
+  export default {
+    name: "App",
+    data() {
+      return {
+        imageName: undefined,
       };
+    },
+    mounted() {
+      JSBridge.registerHandler("onChannelJoinChanged", (e) => {
+        console.log("onChannelJoinChanged", e);
+      });
+      JSBridge.registerHandler("onReceiveFromGroup", (e) => {
+        console.log("onReceiveFromGroup", e);
+      });
+      JSBridge.registerHandler("onReceiveFromPeer", (e) => {
+        console.log("onReceiveFromPeer", e);
+      });
+      JSBridge.registerHandler("onCallUserState", (e) => {
+        console.log("onCallUserState", e);
+      });
+      JSBridge.registerHandler("onCallUserMute", (e) => {
+        console.log("onCallUserMute", e);
+      });
+    },
+    methods: {
+      tirggerFile(event) {
+        //  String ACCEPT_IMAGE = "image";
+        // String ACCEPT_CAMERA = "camera";
+        // String ACCEPT_FILE = "file";
+        console.log("triggerFile", event.target.files);
 
-      fr.readAsText(event.target.files[0], 'UTF-8');
-    },
-    alertAction() {
-      quick.ui.alert({
-        title: '标题',
-        message: '信息',
-        buttonName: '确定',
-        cancelable: 1,
-        success: function() {
-          // 点击 alert的按钮后回调
-        },
-        error: function() {}
-      });
-    },
-    toastAction() {
-      quick.ui.toast({
-        message: 'sd#ddd测试',
-        success: function() {},
-        error: function() {}
-      });
-    },
-    scanAction() {
-      quick.util.scan({
-        success: function(result) {
-          console.log('result', result);
-        },
-        error: function(error) {}
-      });
-    },
-    selectFile() {
-      quick.util.selectFile({
-        success: (result) => {
-          console.log('selectFile', result);
-        }
-      });
-    },
-    openFolder() {
-      quick.util.openFolder({
-        path: '/DCIM',
-        success: (result) => {
-          console.log('openFolder', result);
-        }
-      });
-    },
-    createNewFolder() {
-      quick.util.createFolder({
-        path: '/com/test/test',
-        success: (result) => {
-          console.log('createFolder', result);
-        }
-      });
-    },
-    selectPhoto() {
-      quick.util.selectImage({
-        photoCount: 9,
-        previewEnabled: 1,
-        selectedPhotos: [],
-        success: (result) => {
-          console.log('selectImages', result.resultData);
+        let fr = new FileReader();
 
-          // this.imageName = 'http://localimg' + result.resultData[0];
-          // quick.util.selectFile({
-          //   path: result.resultData[0],
-          //   success: (result) => {
-          //     console.log('selectFile', result);
-          //   }
-          // });
+        fr.onload = function (e) {
+          console.log("e", e);
+        };
 
-          // var fr = new FileReader();
-          // fr.readAsDataURL(this.imageName);
-          // fr.onload = (e) => {
-          //   console.log('fileRead', e);
-          // };
-          // fr.onerror = (e) => {
-          //   console.log('fileRead err', e);
-          // };
-          /**
-         * 选择完图片后返回
-         * {
-                resultData: ["/storage/emulated/0/DCIM/Camera/IMG_20170703_133717.jpg"] // 返回图片的本地路径数组
-           }
-         */
-        },
-        error: function(error) {
-          console.log('errror', error);
-        }
-      });
-    },
-    takePhoto() {
-      quick.util.cameraImage({
-        corp: 1,
-        success: (result) => {
-          console.log('selectImages', result);
-          /**
+        fr.readAsText(event.target.files[0], "UTF-8");
+      },
+      alertAction() {
+        quick.ui.alert({
+          title: "标题",
+          message: "信息",
+          buttonName: "确定",
+          cancelable: 1,
+          success: function () {
+            // 点击 alert的按钮后回调
+          },
+          error: function () {},
+        });
+      },
+      toastAction() {
+        quick.ui.toast({
+          message: "sd#ddd测试",
+          success: function () {},
+          error: function () {},
+        });
+      },
+      scanAction() {
+        quick.util.scan({
+          success: function (result) {
+            console.log("result", result);
+          },
+          error: function (error) {},
+        });
+      },
+      selectFile() {
+        quick.util.selectFile({
+          success: (result) => {
+            console.log("selectFile", result);
+          },
+        });
+      },
+      openFolder() {
+        quick.util.openFolder({
+          path: "/DCIM",
+          success: (result) => {
+            console.log("openFolder", result);
+          },
+        });
+      },
+      // 复制文件到制定文件
+      copyFileTo() {
+        quick.util.copyFileToLocation({
+          oldpath: "",
+          despath: "",
+        });
+      },
+      createNewFolder() {
+        quick.util.createFolder({
+          path: "/com/test/test",
+          success: (result) => {
+            console.log("createFolder", result);
+          },
+        });
+      },
+      // 保存文件到指定名称中，保存在某些本地存储中，不涉及文件夹的操作
+      saveFilesToPath() {
+        quick.util.saveFilePaths({
+          paths: "",
+          saveName: "",
+          success: (result) => {},
+        });
+      },
+      // 对于大文件的处理
+      getMd5List() {
+        quick.uitl.getMd5List({
+          filePath: "",
+          success: (result) => {
+            console.log("md5List", result);
+          },
+        });
+      },
+      // 上传图片或者单个文件
+      uploadImage() {
+        quick.util.uploadImage({
+          reqUrl: "", // url地址
+          type: "", // 请求类型
+          filePath: "", // 文件地址
+          success: (result) => {},
+        });
+      },
+      // 上传md5文件的一部分
+      uploadFilePart() {
+        quick.util.uploadMd5Part({
+          reqUrl: "",
+          type: "",
+          index: 0, // 这个位置是之前保存的一部分的具体位置，如果需要改成md5的可以讲下，或者直接用上面那个试试
+          success: (result) => {},
+        });
+      },
+      selectPhoto() {
+        quick.util.selectImage({
+          photoCount: 9,
+          previewEnabled: 1,
+          selectedPhotos: [],
+          success: (result) => {
+            console.log("selectImages", result.resultData);
+          },
+          error: function (error) {
+            console.log("errror", error);
+          },
+        });
+      },
+      takePhoto() {
+        quick.util.cameraImage({
+          corp: 1,
+          success: (result) => {
+            console.log("selectImages", result);
+            /**
          * 拍完照后返回
          * {
                 resultData: "/storage/emulated/0/DCIM/Camera/IMG_20170703_133717.jpg" // 返回图片的本地路径
            }
          */
-        },
-        error: function(error) {}
-      });
-    },
-    loginWithOutToken() {
-      // 登录不需要token的模块
-      // 这个地方不需要callback
-      quick.taketosee.loginWithOutToken({
-        appId: '511767b5f6974accbae15e9022518589',
-        userId: 'userA',
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    loginWithToken() {
-      quick.taketosee.loginWithUserName({
-        appId: '511767b5f6974accbae15e9022518589',
-        userId: 'userA',
-        token: '',
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    logout() {
-      quick.taketosee.logout({
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    createChannelListenner() {
-      // 登录频道的时候需要先创建 监听
-      quick.taketosee.createChannelListener({
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    joinChannel() {
-      quick.taketosee.joinChannel({
-        channelName: 'channel',
-        success: function(e) {
-          console.log('joinChannel callback', e);
-        },
-        error: function(error) {}
-      });
-    },
-    leaveChannel() {
-      quick.taketosee.leaveChannel({
-        success: function(e) {
-          console.log('leaveChannel callback', e);
-        },
-        error: function(error) {}
-      });
-    },
-    sendGroupMessage() {
-      quick.taketosee.sendGroupMessage({
-        channelText: 'test111',
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    sendPeerMessage() {
-      quick.taketosee.sendPeerMessage({
-        userId: 'userB',
-        peerMessage: 'test peer',
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    initAgroaMessage() {
-      quick.taketosee.initAgoraCall({
-        // 语音appId
-        appId: '3dc9b22d18a8405ea10efc0fcc2054d7',
-        success: function(e) {
-          console.log('initAgroaMessage', e);
-        },
-        error: function(error) {}
-      });
-    },
-    joinCallChannel() {
-      quick.taketosee.joinAgoraCallChannel({
-        // 语音appId
-        accessToken:
-          '0063dc9b22d18a8405ea10efc0fcc2054d7IACSQCZEpK+36X1cgSiXKvm4Xt5SrApUwYkNGixu3X415z37XygAAAAAEAAZP2Q5owMBYQEAAQCiAwFh',
-        channelName: 'voiceDemoChannel1',
-        userId: '100',
-        extraInfo: '',
-        success: function(e) {
-          console.log('joinCallChannel', e);
-        },
-        error: function(error) {}
-      });
-    },
-    leaveCallChannel() {
-      quick.taketosee.leaveAgoraCallChannel({
-        success: function(e) {
-          console.log('leaveCallChannel', e);
-        },
-        error: function(error) {}
-      });
-    },
-    adjustPlayerVolume() {
-      quick.taketosee.adjustPlayerVolume({
-        volume: 0,
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    adjustCallVolume() {
-      quick.taketosee.adjustRecordingVolume({
-        volume: 0,
-        success: function() {},
-        error: function(error) {}
-      });
-    },
-    getBase64Image(img) {
-      var canvas = document.createElement('canvas');
-      canvas.width = img.width;
-      canvas.height = img.height;
+          },
+          error: function (error) {},
+        });
+      },
+      //
+      loginWithOutToken() {
+        // 登录不需要token的模块
+        // 这个地方不需要callback
+        quick.taketosee.loginWithOutToken({
+          appId: "511767b5f6974accbae15e9022518589",
+          userId: "userA",
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      loginWithToken() {
+        quick.taketosee.loginWithUserName({
+          appId: "511767b5f6974accbae15e9022518589",
+          userId: "userA",
+          token: "",
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      logout() {
+        quick.taketosee.logout({
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      createChannelListenner() {
+        // 登录频道的时候需要先创建 监听
+        quick.taketosee.createChannelListener({
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      joinChannel() {
+        quick.taketosee.joinChannel({
+          channelName: "channel",
+          success: function (e) {
+            console.log("joinChannel callback", e);
+          },
+          error: function (error) {},
+        });
+      },
+      leaveChannel() {
+        quick.taketosee.leaveChannel({
+          success: function (e) {
+            console.log("leaveChannel callback", e);
+          },
+          error: function (error) {},
+        });
+      },
+      sendGroupMessage() {
+        quick.taketosee.sendGroupMessage({
+          channelText: "test111",
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      sendPeerMessage() {
+        quick.taketosee.sendPeerMessage({
+          userId: "userB",
+          peerMessage: "test peer",
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      initAgroaMessage() {
+        quick.taketosee.initAgoraCall({
+          // 语音appId
+          appId: "3dc9b22d18a8405ea10efc0fcc2054d7",
+          success: function (e) {
+            console.log("initAgroaMessage", e);
+          },
+          error: function (error) {},
+        });
+      },
+      joinCallChannel() {
+        quick.taketosee.joinAgoraCallChannel({
+          // 语音appId
+          accessToken:
+            "0063dc9b22d18a8405ea10efc0fcc2054d7IACSQCZEpK+36X1cgSiXKvm4Xt5SrApUwYkNGixu3X415z37XygAAAAAEAAZP2Q5owMBYQEAAQCiAwFh",
+          channelName: "voiceDemoChannel1",
+          userId: "100",
+          extraInfo: "",
+          success: function (e) {
+            console.log("joinCallChannel", e);
+          },
+          error: function (error) {},
+        });
+      },
+      leaveCallChannel() {
+        quick.taketosee.leaveAgoraCallChannel({
+          success: function (e) {
+            console.log("leaveCallChannel", e);
+          },
+          error: function (error) {},
+        });
+      },
+      adjustPlayerVolume() {
+        quick.taketosee.adjustPlayerVolume({
+          volume: 0,
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      adjustCallVolume() {
+        quick.taketosee.adjustRecordingVolume({
+          volume: 0,
+          success: function () {},
+          error: function (error) {},
+        });
+      },
+      getBase64Image(img) {
+        var canvas = document.createElement("canvas");
+        canvas.width = img.width;
+        canvas.height = img.height;
 
-      var ctx = canvas.getContext('2d');
-      ctx.drawImage(img, 0, 0, img.width, img.height);
-      var ext = img.src.substring(img.src.lastIndexOf('.') + 1).toLowerCase();
-      var dataURL = canvas.toDataURL('image/' + ext);
-      return dataURL;
-    }
-  }
-};
+        var ctx = canvas.getContext("2d");
+        ctx.drawImage(img, 0, 0, img.width, img.height);
+        var ext = img.src.substring(img.src.lastIndexOf(".") + 1).toLowerCase();
+        var dataURL = canvas.toDataURL("image/" + ext);
+        return dataURL;
+      },
+    },
+  };
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app {
+    font-family: Avenir, Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+    margin-top: 60px;
+  }
 
-.action-list {
-  display: flex;
-  flex-direction: column;
-}
+  .action-list {
+    display: flex;
+    flex-direction: column;
+  }
 
-.action-button {
-  margin-top: 20px;
-}
+  .action-button {
+    margin-top: 20px;
+  }
 </style>
